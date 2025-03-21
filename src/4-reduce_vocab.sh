8e3f97b059ef7032563bd57697d86188c0a35c2a
d-gurgurov <dgurgurov@gmail.com>
2025-03-21 16:29:07 +0100

    updated reduce vocab and compute ppl shell scripts

--- src/4-reduce_vocab.sh
+++ src/4-reduce_vocab.sh
@@ -2,7 +2,12 @@
 
 pip install sentencepiece
 
+# mt - /netscratch/dgurgurov/projects2025/distillation_mbert/models/maltese/distilled_mbert-mt_2_teacher_0.5_last_k_12_2048
+# sk - /netscratch/dgurgurov/projects2025/distillation_mbert/models/slovak/distilled_mbert-sk_2_teacher_0.5_last_k_12_2048
+# sw - /netscratch/dgurgurov/projects2025/distillation_mbert/models/swahili/distilled_mbert-sw_2_teacher_0.5_last_k_12_2048
+
 python reduce_vocab.py --dataset_path "data_maltese" \
-                      --model_name "/netscratch/dgurgurov/projects2025/distillation_mbert/models/maltese/distilled_mbertu_2_double_loss_alpha_0.5_mse_unsoft/checkpoint-50000" \
-                      --tokenizer_name "MLRS/mBERTu" \
-                      --output_name "maltese_model_0.1"
+                      --model_name "/netscratch/dgurgurov/projects2025/distillation_mbert/tiny_models/maltese/distilled_tiny_mbert-mt_truncation_312/checkpoint-50000" \
+                      --tokenizer_name "google-bert/bert-base-multilingual-cased" \
+                      --output_name "distilled_tiny_mbert-mt_truncation_312_vocab_reduce" \
+                      --target_vocab_size 40000
--- src/5-compute_ppl.sh
+++ src/5-compute_ppl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python get_ppl.py --models_path models/maltese \
-                --tokenizer google-bert/bert-base-multilingual-cased \
+python get_ppl.py --models_path /netscratch/dgurgurov/projects2025/distillation_mbert/models/maltese \
                 --language_code mlt_Latn \
-                --output_dir pseudo_perplexity
+                --output_dir pseudo_perplexity \
+                --tokenizer google-bert/bert-base-multilingual-cased \
